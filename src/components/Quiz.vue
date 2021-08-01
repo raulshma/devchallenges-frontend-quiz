12027e7b87709f35b79fda9bdca80e337e0864c0
raulshma <raulshma@gmail.com>
2021-08-01 09:48:32 +0530

    Added footer

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,133 +1,150 @@
 <template>
-  <div class="center-page h-100 d-flex overflow-hidden">
-    <div class="my-card m-auto">
-      <h1 class="display-6 fw-bold text-white mb-3">COUNTRY QUIZ</h1>
-      <div class="card border-radius-lg">
-        <div class="card-body">
-          <img
-            src="../assets/untitled.svg"
-            class="img-fluid position-absolute banner-image"
-          />
-          <div v-if="fetching">
-            <div class="d-flex justify-content-center">
-              <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
+  <div class="d-flex flex-column h-100">
+    <div class="flex-grow-1 d-grid">
+      <div class="center-page my-card m-auto">
+        <h1 class="display-6 fw-bold text-white mb-3">COUNTRY QUIZ</h1>
+        <div class="card border-radius-lg">
+          <div class="card-body">
+            <img
+              src="../assets/untitled.svg"
+              class="img-fluid position-absolute banner-image"
+            />
+            <div v-if="fetching">
+              <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            v-if="
-              !fetching &&
-              questions.length > 0 &&
-              (state.completed === 0 || state.completed === -1)
-            "
-            class="d-flex flex-column gap-3"
-          >
-            <transition name="fade">
-              <div v-if="questions[state.currentQuestionIndex].flag != null">
-                <img
-                  :src="questions[state.currentQuestionIndex].flag"
-                  class="img-fluid zoom-in shadow"
-                  style="height: 75px"
-                  alt="Question flag image here"
-                />
+            <div
+              v-if="
+                !fetching &&
+                questions.length > 0 &&
+                (state.completed === 0 || state.completed === -1)
+              "
+              class="d-flex flex-column gap-3"
+            >
+              <transition name="fade">
+                <div v-if="questions[state.currentQuestionIndex].flag != null">
+                  <img
+                    :src="questions[state.currentQuestionIndex].flag"
+                    class="img-fluid zoom-in shadow"
+                    style="height: 75px"
+                    alt="Question flag image here"
+                  />
+                </div>
+              </transition>
+              <h2 class="h4 text-primary fw-bolder">
+                {{ questions[state.currentQuestionIndex].question }}
+              </h2>
+              <div class="d-grid gap-4">
+                <template
+                  v-for="(option, i) in questions[state.currentQuestionIndex]
+                    .answers"
+                  :key="i"
+                >
+                  <button
+                    @click="answerCheck(option, i)"
+                    :disabled="state.buttonsDisabled"
+                    :class="`btn ${
+                      state.correctAnswerIndex === i
+                        ? 'btn-success'
+                        : state.selectedAnswerIndex === i
+                        ? 'btn-danger'
+                        : 'btn-outline-primary'
+                    } d-inline-flex gap-4 fw-bold shadow-sm hover-orange border-radius-md p-3`"
+                  >
+                    <p class="my-auto ms-2">{{ optionsChars[i] }}</p>
+                    <p class="my-auto">{{ option.name }}</p>
+                    <span
+                      v-if="state.selectedAnswerIndex !== -1"
+                      class="flex-grow-1 d-flex flex-row-reverse"
+                      ><i
+                        :class="`bi ${
+                          state.correctAnswerIndex === i
+                            ? 'bi-check-circle'
+                            : state.selectedAnswerIndex === i
+                            ? 'bi-x-circle'
+                            : ''
+                        }`"
+                      ></i
+                    ></span>
+                  </button>
+                </template>
               </div>
-            </transition>
-            <h2 class="h4 text-primary fw-bolder">
-              {{ questions[state.currentQuestionIndex].question }}
-            </h2>
-            <div class="d-grid gap-4">
-              <template
-                v-for="(option, i) in questions[state.currentQuestionIndex]
-                  .answers"
-                :key="i"
-              >
+              <div class="ms-auto">
                 <button
-                  @click="answerCheck(option, i)"
-                  :disabled="state.buttonsDisabled"
-                  :class="`btn ${
-                    state.correctAnswerIndex === i
-                      ? 'btn-success'
-                      : state.selectedAnswerIndex === i
-                      ? 'btn-danger'
-                      : 'btn-outline-primary'
-                  } d-inline-flex gap-4 fw-bold shadow-sm hover-orange border-radius-md p-3`"
-                >
-                  <p class="my-auto ms-2">{{ optionsChars[i] }}</p>
-                  <p class="my-auto">{{ option.name }}</p>
-                  <span
-                    v-if="state.selectedAnswerIndex !== -1"
-                    class="flex-grow-1 d-flex flex-row-reverse"
-                    ><i
-                      :class="`bi ${
-                        state.correctAnswerIndex === i
-                          ? 'bi-check-circle'
-                          : state.selectedAnswerIndex === i
-                          ? 'bi-x-circle'
-                          : ''
-                      }`"
-                    ></i
-                  ></span>
-                </button>
-              </template>
-            </div>
-            <div class="ms-auto">
-              <button
-                v-if="
-                  state.selectedAnswerIndex !== -1 &&
-                  state.completed !== -1 &&
-                  state.currentQuestionIndex !== 9
-                "
-                class="btn btn-warning fw-bold border-radius-md"
-                @click="nextQuestion()"
-              >
-                Next
-              </button>
-              <button
-                v-if="state.completed === -1"
-                class="btn btn-warning fw-bold border-radius-md"
-                @click="showResult()"
-              >
-                Result
-              </button>
-            </div>
-          </div>
-          <div
-            v-if="state.completed === 1"
-            style="height: 250px"
-            class="d-flex flex-column"
-          >
-            <div class="m-auto">
-              <h1 class="display-3 fw-bolder text-center">Results</h1>
-              <p class="fw-normal text-secondary text-center">
-                You got
-                <span
-                  :class="`fw-bolder fs-3 px-1 ${
-                    state.score > 0 ? 'text-success' : 'text-danger'
-                  }`"
-                  >{{ state.score }}</span
-                >
-                correct answers
-              </p>
-              <div class="text-center mt-5">
-                <button
-                  class="
-                    shadow-sm
-                    btn-lg btn-outline-primary
-                    fw-bold
-                    fs-6
-                    border-radius-md
+                  v-if="
+                    state.selectedAnswerIndex !== -1 &&
+                    state.completed !== -1 &&
+                    state.currentQuestionIndex !== 9
                   "
-                  @click="reset()"
+                  class="btn btn-warning fw-bold border-radius-md"
+                  @click="nextQuestion()"
                 >
-                  Try Again
+                  Next
                 </button>
+                <button
+                  v-if="state.completed === -1"
+                  class="btn btn-warning fw-bold border-radius-md"
+                  @click="showResult()"
+                >
+                  Result
+                </button>
+              </div>
+            </div>
+            <div
+              v-if="state.completed === 1"
+              style="height: 250px"
+              class="d-flex flex-column"
+            >
+              <div class="m-auto">
+                <h1 class="display-3 fw-bolder text-center">Results</h1>
+                <p class="fw-normal text-secondary text-center">
+                  You got
+                  <span
+                    :class="`fw-bolder fs-3 px-1 ${
+                      state.score > 0 ? 'text-success' : 'text-danger'
+                    }`"
+                    >{{ state.score }}</span
+                  >
+                  correct answers
+                </p>
+                <div class="text-center mt-5">
+                  <button
+                    class="
+                      shadow-sm
+                      btn-lg btn-outline-primary
+                      fw-bold
+                      fs-6
+                      border-radius-md
+                    "
+                    @click="reset()"
+                  >
+                    Try Again
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <footer class="footer mt-auto py-1 bg-light">
+      <div class="d-flex justify-content-center gap-2">
+        <p class="mb-0">created by raulshma</p>
+        <a href="https://github.com/raulshma" target="__blank" rel="noopener">
+          <i className="bi bi-github" />
+        </a>
+        <a
+          href="https://devchallenges.io/portfolio/raulshma"
+          target="__blank"
+          rel="noopener"
+        >
+          devChallenges.io
+        </a>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -221,6 +238,9 @@ export default {
 </script>
 
 <style>
+.center-page {
+  place-items: center;
+}
 .border-radius-md {
   border-radius: 0.75rem !important;
 }
